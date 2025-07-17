bb99ddaaf93f0caaec9043eb7fcc5bc3b7f61ad2
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2025-07-17 09:54:27 +0100

    SDIT-2942 Upgrade to HMPPS gradle plugin 8.4.2 (#478)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 @file:Suppress("UnstableApiUsage")
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.2"
   kotlin("plugin.spring") version "2.2.0"
 }
 
