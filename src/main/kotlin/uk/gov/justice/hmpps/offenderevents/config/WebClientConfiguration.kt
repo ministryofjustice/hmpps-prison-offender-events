96d19ee502199a3c7a30644ebd667cae7e7398aa
karenmillermoj <karen.miller3@justice.gov.uk>
2025-12-19 18:19:41 +0000

    SDIT-3215 Remove unnecessary builder

--- src/main/kotlin/uk/gov/justice/hmpps/offenderevents/config/WebClientConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/offenderevents/config/WebClientConfiguration.kt
@@ -17,9 +17,6 @@ class WebClientConfiguration(
   @Value("\${api.timeout:20s}") val timeout: Duration,
 ) {
   @Bean
-  fun webClientBuilder(): WebClient.Builder = WebClient.builder()
-
-  @Bean
   fun prisonApiWebClient(
     authorizedClientManager: OAuth2AuthorizedClientManager,
     builder: WebClient.Builder,
