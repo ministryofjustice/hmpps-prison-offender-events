79934a70ba55e8c8cd78e8d6765de97a4938b278
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2026-01-23 14:10:48 +0000

    SDIT-3361: ⬆️ Upgrade to spring boot 4.0.2 etc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 @file:Suppress("UnstableApiUsage")
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.0.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.0.1"
   kotlin("plugin.spring") version "2.3.0"
 }
 
