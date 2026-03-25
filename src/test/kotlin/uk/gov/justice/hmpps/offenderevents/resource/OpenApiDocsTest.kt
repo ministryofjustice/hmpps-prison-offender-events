4abad6a75cdbf9e0f2bf3667521a2af805d7e8d2
petergphillips <5099053+petergphillips@users.noreply.github.com>
2026-03-25 15:51:59 +0000

    SDIT-3616: ✨ Secure retry-all-dlqs endpoint and remove retry cronjob (#545)

--- src/test/kotlin/uk/gov/justice/hmpps/offenderevents/resource/OpenApiDocsTest.kt
+++ src/test/kotlin/uk/gov/justice/hmpps/offenderevents/resource/OpenApiDocsTest.kt
@@ -83,7 +83,7 @@ class OpenApiDocsTest : IntegrationTestBase() {
   }
 
   @ParameterizedTest
-  @CsvSource(value = ["bearer-jwt, ROLE_QUEUE_ADMIN"])
+  @CsvSource(value = ["bearer-jwt, ROLE_SYSCON__QUEUE_ADMIN"])
   fun `the security scheme is setup for bearer tokens`(key: String, role: String) {
     webTestClient.get()
       .uri("/v3/api-docs")
