1c5da052d6a26447bd55fb1645c83604a84da948
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2026-06-03 11:43:00 +0100

    SDIT-3760 prison-offender-events.prisoner.received should not be suppressed by a TAP (#569)
    * SDIT-3760 prison-offender-events.prisoner.received should not be suppressed by a TAP
    
    * SDIT-3760 prison-offender-events.prisoner.received should not be suppressed by a TAP

--- src/main/kotlin/uk/gov/justice/hmpps/offenderevents/services/ReceivePrisonerReasonCalculator.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/offenderevents/services/ReceivePrisonerReasonCalculator.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.hmpps.offenderevents.services
 
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
+import uk.gov.justice.hmpps.offenderevents.services.CurrentLocation.IN_PRISON
 import uk.gov.justice.hmpps.offenderevents.services.MovementReason.TRANSFER
 
 @Component
@@ -12,12 +13,24 @@ class ReceivePrisonerReasonCalculator(
     val prisonerDetails = prisonApiService.getPrisonerDetails(offenderNumber)
 
     val reason = when (prisonerDetails.typeOfMovement()) {
-      MovementType.TEMPORARY_ABSENCE -> Reason.TEMPORARY_ABSENCE_RETURN
-      MovementType.COURT -> Reason.RETURN_FROM_COURT
+      // They might not actually be in prison if they were received then went straight out on a TAP
+      // or to court within the (usually 45 mins) grace period
+
+      MovementType.TEMPORARY_ABSENCE -> when (prisonerDetails.currentLocation()) {
+        IN_PRISON -> Reason.TEMPORARY_ABSENCE_RETURN
+        else -> Reason.ADMISSION
+      }
+
+      MovementType.COURT -> when (prisonerDetails.currentLocation()) {
+        IN_PRISON -> Reason.RETURN_FROM_COURT
+        else -> Reason.ADMISSION
+      }
+
       MovementType.ADMISSION -> when (prisonerDetails.movementReason()) {
         TRANSFER -> Reason.TRANSFERRED
         else -> Reason.ADMISSION
       }
+
       else -> Reason.ADMISSION
     }
 
@@ -49,7 +62,7 @@ class ReceivePrisonerReasonCalculator(
     val nomisMovementReason: MovementReason,
   ) : PrisonerMovementReason {
     fun hasPrisonerActuallyBeenReceived(): Boolean {
-      if ((currentLocation == CurrentLocation.IN_PRISON) != (currentPrisonStatus == CurrentPrisonStatus.UNDER_PRISON_CARE)) {
+      if ((currentLocation == IN_PRISON) != (currentPrisonStatus == CurrentPrisonStatus.UNDER_PRISON_CARE)) {
         log.warn("hasPrisonerActuallyBeenReceived(): verdict based on active differs from old for $this")
       }
       return currentPrisonStatus == CurrentPrisonStatus.UNDER_PRISON_CARE
