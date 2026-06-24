b9e96aa5934613966130b72b88ec76412a00b605
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2026-06-24 08:12:13 +0100

    SDIT-3916: ⬆️ Upgrade gradle plugin for dom purify suppressions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 @file:Suppress("UnstableApiUsage")
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.5.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.5.2"
   kotlin("plugin.spring") version "2.4.0"
 }
 
