9420ec626f75ffc327b9448df8b17d4a2ba82325
petergphillips <5099053+petergphillips@users.noreply.github.com>
2026-07-08 08:36:38 +0100

    SDIT-4019: 🔧 Missed suppression removal off (#580)

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,10 +14,6 @@ configurations {
 testing {
   suites {
     register<JvmTestSuite>("testSmoke") {
-      dependencyCheck {
-        suppressionFiles.add("azure-dependency-check-suppress.xml")
-      }
-
       dependencies {
         implementation(project())
       }
